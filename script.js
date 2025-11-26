eaf0ccbcd13f4f11b75141483aed02f496e1cf86
R-Sagrazzini <richysag22@gmail.com>
2025-11-26 10:37:08 +0100

    Prime modifiche sito

---
+++ script.js
@@ -0,0 +1,7 @@
+function elabora() {
+    let a = document.getElementById("inNumber").value;
+    alert(a);
+}
+function annulla(){
+    document.getElementById("inNumber").value = "";
+}               
